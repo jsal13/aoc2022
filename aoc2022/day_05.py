e37936b44f923b04ad917882ded0ffe10c11f457
James Salvatore <jimmy.c.salvatore@gmail.com>
2022-12-06 00:07:30 -0600

    feat: day 5;

--- aoc2022/day_05.py
+++ aoc2022/day_05.py
@@ -1,5 +1,5 @@
+import copy
 import re
-from queue import SimpleQueue
 from typing import List
 
 import numpy as np
@@ -13,10 +13,7 @@ class InputParser:
     def __init__(self, data: str):
         self.data = data
         self.queue_raw, self.instructions_raw = self._split_queue_and_instructions()
-
-        self.queue_length = self._get_queue_length()
         self.queue = self._parse_queue()
-
         self.instructions = self._parse_instructions()
 
     def _split_queue_and_instructions(self) -> tuple[str, str]:
@@ -24,62 +21,62 @@ class InputParser:
         queues_raw, instructions_raw = self.data.split("\n\n")
         return queues_raw, instructions_raw
 
-    def _get_queue_length(self) -> int:
-        """Get the length of the queue."""
-        digits_strs = re.findall(r"([0-9]+) ", self.queue_raw)  # Gets all digits as strs.
-        return max(map(int, digits_strs))
-
     def _parse_queue(self) -> List[List[str]]:
         """
         Parse queue portion of the data.
 
         Notes
         -----
-        The regex does the following:
-        - Removes all numbers,
-        - Removes all brackets and replaces with spaces,
-        - If any letter is preceeded by three spaces, replaces with just that letter.
-            - This moves the letters to the left the appropriate amount.
-        - Collapses any remaining 3-space intervals.
-        - Removes initial space at the beginning of the lines.
-        - Truncates each line to match the queue_length.
-
+        Omg, make this better.
         """
-        queue_letters_only = re.sub(r"[0-9]", "", self.queue_raw)
-        queue_letters_only = re.sub(r"[\[\]]", " ", queue_letters_only)
-        queue_letters_only = re.sub(r"   ([A-Z])", "\\1", queue_letters_only)
-        queue_letters_only = re.sub(r"^ ", "", queue_letters_only)
-        queue_letters_only = re.sub(r"   ", "", queue_letters_only)
-        queue_letters_only = re.sub(r"\n ", "\n", queue_letters_only)
-        queue_letters_list = [
-            list(line) for line in queue_letters_only.split("\n") if line.strip()
-        ]
-        queue_letters_list.reverse()
+        queue_lines = self.queue_raw.splitlines()
+        queue_lines = queue_lines[:-1]  # Removes numbers line.
+        queue_lines = [
+            line for line in queue_lines if line.strip()
+        ]  # Removes blank lines.
 
-        queue_list = [[] for _ in range(self.queue_length)]
-        for line in queue_letters_list:
-            for col, letter in enumerate(line):
-                if letter.strip():  # If not a space...
-                    queue_list[col].append(letter)
+        queue_letters = []
+        for line in queue_lines:
+            new_line = []
+            for idx in range(0, len(line), 4):
+                new_line.append(line[idx : idx + 4])
 
-        for q in queue_list:
-            print(q)
-        return queue_list
+            # Remove extra spaces and brackets.
+            new_line = [re.sub(r"[ \[\]]", "", item) for item in new_line]
+            queue_letters.append(new_line)
 
-    def _parse_instructions(self) -> List[tuple[int, int, int]]:
+        # Find the largest row, extend all other rows to match that length.
+        max_row_length = len(max(queue_letters, key=len))
+
+        queue_letters_matrix = []
+        for row in queue_letters:
+            empty_space = [""] * (max_row_length - len(row))
+            queue_letters_matrix.append([*row, *empty_space])
+
+        queue_letters_matrix = np.transpose(np.array(queue_letters_matrix)).tolist()
+
+        queue = []
+        for row in queue_letters_matrix:
+            queue.append([letter for letter in row[::-1] if letter != ""])
+
+        return queue
+
+    def _parse_instructions(self) -> List[List[int]]:
         """Parse instructions portion of the data."""
         return [
-            tuple(map(int, val))
+            list(map(int, val))
             for line in self.instructions_raw.splitlines()
             for val in re.findall(r"move (\d+) from (\d+) to (\d+)", line)
         ]
 
 
 def execute_instructions_on_queue(
-    queue: List[List[str]], instructions: tuple[int, int, int]
+    queue: List[List[str]], instructions: List[List[int]], bulk_move: bool = False
 ) -> List[List[str]]:
     """
-    Execute instructions **in place** on ``queue``.
+    Execute instructions on ``queue``.
+
+    ``bulk_move`` implies each are moved at once, instead of one at a time.
 
     Notes
     -----
@@ -88,17 +85,23 @@ def execute_instructions_on_queue(
     adjusting for counting starting at 0.
 
     """
+    tmp_queue = copy.deepcopy(queue)  # To prevent overwriting queue.
     for instruction in instructions:
         iter_number = instruction[0]
         column_from = instruction[1] - 1
         column_to = instruction[2] - 1
 
-        for _ in range(iter_number):
-            val = queue[column_from].pop()
-            queue[column_to].append(val)
-        print(queue)
+        if bulk_move:
+            vals = tmp_queue[column_from][-1 * iter_number :]
+            for _ in range(iter_number):
+                tmp_queue[column_from].pop()
+            tmp_queue[column_to] = [*tmp_queue[column_to], *vals]
+        else:
+            for _ in range(iter_number):
+                val = tmp_queue[column_from].pop()
+                tmp_queue[column_to].append(val)
 
-    return queue
+    return tmp_queue
 
 
 def get_top_values_in_queue(queue: List[List[str]]) -> List[str]:
@@ -106,34 +109,21 @@ def get_top_values_in_queue(queue: List[List[str]]) -> List[str]:
     return [row[-1] for row in queue]
 
 
-DATA = """
-    [D]
-[N] [C]    
-[Z] [M] [P]
- 1   2   3 
+if __name__ == "__main__":
+    raw_data = read_aoc_day_data_file(5)
 
-move 1 from 2 to 1
-move 3 from 1 to 3
-move 2 from 2 to 1
-move 1 from 1 to 2"""
+    # Part 1:
+    parsed_data = InputParser(raw_data)
+    problem_queue = parsed_data.queue
+    problem_instructions = parsed_data.instructions
+    result = execute_instructions_on_queue(
+        queue=problem_queue, instructions=problem_instructions, bulk_move=False
+    )
 
-parsed_data = InputParser(DATA)
-result = execute_instructions_on_queue(
-    queue=parsed_data.queue, instructions=parsed_data.instructions
-)
+    print(f"Part 1: {''.join(i[-1] for i in result)}")
 
-
-# if __name__ == "__main__":
-# raw_data = read_aoc_day_data_file(5)
-# parsed_data = InputParser(raw_data)
-# problem_queue = parsed_data.queue
-# problem_instructions = parsed_data.instructions
-# result = execute_instructions_on_queue(
-#     queue=problem_queue, instructions=problem_instructions
-# )
-
-# # Part 1.
-# print(f"Part 1: {''.join(result)}")
-
-#     # Part 2.
-#     print(f"Part 2: {pairings.count_overlapping_pairs()}")
+    # Part 2:
+    result = execute_instructions_on_queue(
+        queue=problem_queue, instructions=problem_instructions, bulk_move=True
+    )
+    print(f"Part 2: {''.join(i[-1] for i in result)}")
--- tests/test_day_05.py
+++ tests/test_day_05.py
@@ -16,15 +16,15 @@ from aoc2022.day_05 import (
 @pytest.fixture(name="test_data_raw")
 def test_data_raw_fixture():
     return """
-    [D]    
-[N] [C]    
+    [D]
+[N] [C]
 [Z] [M] [P]
- 1   2   3 
+ 1   2   3
 
 move 1 from 2 to 1
 move 3 from 1 to 3
 move 2 from 2 to 1
-move 1 from 1 to 2""".strip()
+move 1 from 1 to 2"""
 
 
 @pytest.fixture(name="test_data_queue")
@@ -33,8 +33,8 @@ def test_data_queue_fixture():
 
 
 @pytest.fixture(name="test_data_instructions")
-def test_data_instructions_fixture() -> List[tuple[int, int, int]]:
-    return [(1, 2, 1), (3, 1, 3), (2, 2, 1), (1, 1, 2)]
+def test_data_instructions_fixture() -> List[List[int]]:
+    return [[1, 2, 1], [3, 1, 3], [2, 2, 1], [1, 1, 2]]
 
 
 @pytest.fixture(name="class_input_parser")
@@ -49,22 +49,33 @@ def test_input_parser_parse_instructions(
     assert class_input_parser._parse_instructions() == test_data_instructions
 
 
-def test_input_parser_get_queue_length(class_input_parser: InputParser):
-    assert class_input_parser._get_queue_length() == 3
-
-
 def test_input_parser_parse_queue(class_input_parser: InputParser, test_data_queue):
     assert class_input_parser._parse_queue() == test_data_queue
 
 
-def test_execute_instructions_on_queue(class_input_parser: InputParser):
+def test_execute_instructions_on_queue_bulk_move_false(class_input_parser: InputParser):
     queue = class_input_parser.queue
     instructions = class_input_parser.instructions
 
-    assert execute_instructions_on_queue(queue=queue, instructions=instructions) == [
+    assert execute_instructions_on_queue(
+        queue=queue, instructions=instructions, bulk_move=False
+    ) == [
         ["C"],
         ["M"],
         ["P", "D", "N", "Z"],
+    ]
+
+
+def test_execute_instructions_on_queue_bulk_move_true(class_input_parser: InputParser):
+    queue = class_input_parser.queue
+    instructions = class_input_parser.instructions
+
+    assert execute_instructions_on_queue(
+        queue=queue, instructions=instructions, bulk_move=True
+    ) == [
+        ["M"],
+        ["C"],
+        ["P", "Z", "N", "D"],
     ]
 
 
